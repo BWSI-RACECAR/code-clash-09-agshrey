96c89613311eb0967a0d821de1f4438acb0760d5
agshrey <68401509+agshrey@users.noreply.github.com>
2022-07-21 10:32:45 -0400

    Add files via upload

--- licenseplate.py
+++ licenseplate.py
@@ -28,18 +28,37 @@ class Solution:
         # return: int
         
         # TODO: Write code below to return an int with the solution to the prompt
-        letterCount = 3-sum(c.isalpha() for c in str)
-        l = sum(c.isalpha() for c in str)
-        digitCount = 4-sum(c.isdigit() for c in str)
+        # letterCount = 3-sum(c.isalpha() for c in str)
+        # l = sum(c.isalpha() for c in str)
+        # digitCount = 4-sum(c.isdigit() for c in str)
+        # d = sum(c.isdigit() for c in str)
+        # a = []
+        # for s in str:
+        #     if s == "." and letterCount != 0:
+        #         a.append(26-l)
+        #         letterCount += 1
+        #         l+=1
+        #     elif s == "." and digitCount != 0:
+        #         a.append(10-d)
+        #         digitCount+=1
+        #         d+=1
+        # total = 1
+        # print(a)
+        # for i in a:
+        #     total *= i
+        # return total
         d = sum(c.isdigit() for c in str)
+        l = sum(c.isalpha() for c in str)
+        letters = 26-l
+        digits = 10-d
         a = []
-        for s in str:
-            if s == "." and letterCount != 0:
-                a.append(26-l)
-                letterCount += 1
-            elif s == "." and digitCount != 0:
-                a.append(10-d)
-                digitCount+=1
+        for b in range(d, 4):
+            a.append(digits)
+            digits -= 1
+        for c in range(l, 3):
+            a.append(letters)
+            letters -= 1
+        
         total = 1
         for i in a:
             total *= i
